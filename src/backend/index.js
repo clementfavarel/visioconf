da080db3214e0a51bb79185a2cb9db2f0b1827a3
antoineleclerc <plkode.dev@protonmail.com>
2024-02-29 21:35:04 +0100

    updated index.js backend to delete the route (handled by the index.js routes file

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,10 +3,6 @@ const app = require("./app");
 const { createServer } = require("http");
 const server = createServer(app);
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
-
 const port = normalizePort(process.env.PORT || "3001");
 app.set("port", port);
 
@@ -16,14 +12,11 @@ server.listen(port, () => {
 
 function normalizePort(val) {
     const port = parseInt(val, 10);
-
     if (isNaN(port)) {
         return val;
     }
-
     if (port >= 0) {
         return port;
     }
-
     return false;
 }
